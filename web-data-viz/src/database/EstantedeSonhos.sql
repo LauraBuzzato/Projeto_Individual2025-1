9b0490597aff44ad6c90ebf8850415623d09ea99
LauraBuzzato <laura.buzzato@sptech.school>
2025-05-10 17:32:37 -0300

    adicionando web data viz

---
+++ web-data-viz/src/database/EstantedeSonhos.sql
@@ -0,0 +1,23 @@
+create database estantedesonhos;
+use estantedesonhos;
+
+create table usuario(
+id int not null primary key auto_increment,
+nome varchar(45),
+email varchar(45),
+senha varchar(45));
+
+create table livro(
+id int not null primary key auto_increment,
+nome varchar(45),
+genero varchar(45),
+qtdPagina int, 
+urlCapa varchar(100));
+
+create table lido(
+fkusuario int not null,
+fklivro int not null,
+avaliacao varchar(200),
+primary key (fkusuario, fklivro),
+constraint fkusuario foreign key (fkusuario) references usuario(id),
+constraint fklivro foreign key (fklivro) references livro(id));
